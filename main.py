458047b187a898df896f83a783477dd85e97227d
ttema4 <08012005ArIb>
2021-04-26 10:05:50 +0300

    homepage

--- main.py
+++ main.py
@@ -11,11 +11,24 @@ db = SQLAlchemy(app)
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
     price = db.Column(db.Integer, nullable=False)
-    path = db.Column(db.String, nullable=False)
+    pic = db.Column(db.String, nullable=False)
 
     def __str__(self):
-        return str(self.id) + ' ' + self.title + ' ' + str(self.price) + ' ' + self.path
+        return str(self.id) + '/' + self.title + '/' + self.description + '/' + str(self.price) + '/' + self.pic
+
+    def get_title(self):
+        return self.title
+
+    def get_desc(self):
+        return self.description
+
+    def get_price(self):
+        return ''.join([str(self.price), '$'])
+
+    def get_pic(self):
+        return self.pic
 
 
 class User(db.Model):
@@ -62,15 +75,15 @@ def loginpage():
         data = request.form.to_dict()
         un = data['login']
         pw = data['password']
+        if un == 'admin' and pw == 'admin':
+            return redirect('/admin')
         name = False
         pas = False
-        cur_user = None
         for user in User.query.all():
             if str(user).split()[1] == un:
                 name = True
                 if str(user).split()[2] == pw:
                     pas = True
-                    cur_user = user
                     break
         if name and pas:
             return redirect('/home')
@@ -83,7 +96,19 @@ def loginpage():
 @app.route('/home', methods=['GET'])
 def homepage():
     if request.method == 'GET':
-        return render_template('homepage.html', username="user")
+        return render_template('homepage.html', username="user", items=Item.query.all())
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def adminpage():
+    if request.method == 'GET':
+        return render_template('adminpage.html')
+    elif request.method == 'POST':
+        data = request.form.to_dict()
+        item = Item(title=data['name'], description=data['desc'], price=data['cost'], pic=data['pic'])
+        db.session.add(item)
+        db.session.commit()
+        return redirect('/admin')
 
 
 def main():
